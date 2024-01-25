58328c7ef6da79630263438515d14e8dbe54bde7
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-25 15:07:35 -0600

    Exposing component's APIs with useImperativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import Form from './components/Form'
+import Form, { type FormHandle} from './components/Form'
 import Input from './components/Input'
 import Button from './components/Button'
 import './App.css'
+import { useRef } from 'react'
 
 function App() {
+  const customForm = useRef<FormHandle>(null)
 
   function handleSave(data: unknown) {
     const extractedData = data as { name: string, age: string }
     console.log(extractedData)
+    customForm.current?.clear()
   }
 
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input label='Name' type='text' id='name' />
         <Input label='Age' type='number' id='age' />
         <p>
