8125704894314165e679264bd45edaf80be974bd
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-27 21:06:57 -0600

    Added progress bar to display remaining time (progress has a flaw)

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,12 +1,19 @@
 import Container from './UI/Container'
 import { type Timer as TimerProps } from '../store/timers-context'
+import { useState } from 'react'
 
 
 export default function Timer({ name, duration }: TimerProps) {
+    const [remainingTime, setRemainingTime] = useState(duration * 1000)
+
+    setInterval(function () {
+        setRemainingTime(prevTime => prevTime - 50)
+     }, 50)
+
     return (
         <Container as="article">
             <h2>{name}</h2>
-            <p>{duration}</p>
+            <p><progress max={ duration * 1000} value={remainingTime } /></p>
         </Container>
     )
 }
