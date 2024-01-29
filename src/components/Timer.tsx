9485ef381133cdeea7d8c753736e40d5808ff539
Eduardo Gochi <eduardo.gochi@gmail.com>
2024-01-29 12:22:11 -0600

    Fixed bug with timers restarting after they had already been expired

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,12 +3,10 @@ import { useEffect, useRef, useState } from 'react'
 import Container from './UI/Container'
 import { useTimersContext, type Timer as TimerProps } from '../store/timers-context'
 
-
 export default function Timer({ name, duration }: TimerProps) {
     const interval = useRef<number | null>(null)
     const [remainingTime, setRemainingTime] = useState(duration * 1000)
     const { isRunning } = useTimersContext()
-
 
     if (remainingTime <= 0 && interval.current) {
         clearInterval(interval.current)
@@ -16,11 +14,18 @@ export default function Timer({ name, duration }: TimerProps) {
 
     useEffect(() => {
         let timer: number
+
         if (isRunning) {
-            const timer = setInterval(function () {
-                setRemainingTime(prevTime => prevTime - 50)
+            timer = setInterval(function () {
+                setRemainingTime((prevTime) => {
+                    if (prevTime <= 0) {
+                        return prevTime
+                    }
+                    return prevTime - 50
+                })
             }, 50)
             interval.current = timer
+
         } else if (interval.current) {
             clearInterval(interval.current)
         }
@@ -33,7 +38,9 @@ export default function Timer({ name, duration }: TimerProps) {
     return (
         <Container as="article">
             <h2>{name}</h2>
-            <p><progress max={duration * 1000} value={remainingTime} /></p>
+            <p>
+                <progress max={duration * 1000} value={remainingTime} />
+            </p>
             <p>{formattedRemainingTime}</p>
         </Container>
     )
